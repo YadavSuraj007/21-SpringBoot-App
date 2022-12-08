ec16504e6e2bbb6b6b341633ca5326797dd36084
YadavSuraj007 <surajyadav063@gmail.con>
2022-12-08 15:28:06 +0530

    HIS-2000:Changes

--- src/main/java/com/nareshit/helloworl/HelloWorldController.java
+++ src/main/java/com/nareshit/helloworl/HelloWorldController.java
@@ -11,5 +11,10 @@ public class HelloWorldController {
 	public String getHelloWorld() {
 		return "Welcome to Spring Boot Through STS And Git repository // changes for TECH-M 122//";
 	}
+           // HIS-2000 related changes
+         public void doProcess(){
+            // logic goes here 
+      
+}
 
 }
